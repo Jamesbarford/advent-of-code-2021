f49bfa43b92a632d19ee3e585cc42d8dded99ef6
Jamesbarford <jamesbarfordevans@gmail.com>
2021-12-16 20:42:10 +0000

    day15 pt1

--- day15/main.c
+++ day15/main.c
@@ -50,7 +50,7 @@ void matrixRelease(matrix *m) {
 void matrixprint(matrix *m) {
     for (int y = 0; y < m->rows; ++y) {
         for (int x = 0; x < m->columns; ++x)
-            printf("%d", m->entries[x + y * m->columns]);
+            printf("%2d ", m->entries[x + y * m->columns]);
         printf("\n");
     }
     printf("\n");
@@ -110,52 +110,53 @@ heapNode *heapNodeNew(int weight, int x, int y) {
     return hn;
 }
 
-typedef int heapNodeCmp(heapNode *hn1, heapNode *hn2);
+void heapNodePrint(void *_hn) {
+    heapNode *hn = _hn;
+    printf("x:%d, y:%d, weight:%d\n", hn->x, hn->y, hn->weight);
+}
 
-// this is the implementation of a simple priority queue
+int weightcmp(void *_hn1, void *_hn2) {
+    heapNode *hn1, *hn2;
+    hn1 = _hn1;
+    hn2 = _hn2;
+
+    if (hn1->weight >= hn2->weight)
+        return -1;
+    return 1;
+}
+
 typedef struct heap {
     int size;
-    int capacity;
-    heapNodeCmp *compare;
-    heapNode **entries;
+    // Keep it simple lets just use ints
+    int (*cmp)(void *, void *);
+    void (*freeValue)(void *);
+    void (*print)(void *);
+    void **entries;
 } heap;
-
-int weightcmp(heapNode *hn1, heapNode *hn2) {
-    return hn1->weight < hn2->weight;
-}
-
-/* Treat the entries as a binary tree, despite it being a flat array */
 #define heapParent(p) ((int)(((p)-1) / 2))
-#define heapLeft(p) (((p)*2) + 1)
-#define heapRight(p) (((p)*2) + 2)
+#define heapLeft(p)   (((p)*2) + 1)
+#define heapRight(p)  (((p)*2) + 2)
 
-heap *heapNew(heapNodeCmp *compare) {
-    heap *hp;
-    hp = xmalloc(sizeof(heap));
-    hp->capacity = 1 << 30; // this should be more than big enough
-    hp->entries = xcalloc(hp->capacity, sizeof(heapNode *));
-    hp->size = 0;
-    hp->compare = compare;
-    return hp;
+heap *heapNew(int (*cmp)(void *, void *), void (*freeValue)(void *)) {
+    heap *h;
+    h = xmalloc(sizeof(heap));
+    h->entries = xcalloc(1 << 30, sizeof(void *));
+    h->size = 0;
+    h->cmp = cmp;
+    h->freeValue = freeValue;
+    return h;
 }
 
-void heapNodeAssign(heapNode *hn, int weight, int x, int y) {
-    hn->x = x;
-    hn->y = y;
-    hn->weight = weight;
-}
-
-int heapInsert(heap *hp, heapNode *new) {
+void heapInsert(heap *hp, void *entry) {
     int insert_pos, parent_pos;
-    heapNode *tmp;
+    void *tmp;
 
-    hp->entries[hp->size] = new;
-
+    hp->entries[hp->size] = entry;
     insert_pos = hp->size;
     parent_pos = heapParent(insert_pos);
 
     while (insert_pos > 0 &&
-            hp->compare(hp->entries[parent_pos], hp->entries[insert_pos]))
+            hp->cmp(hp->entries[parent_pos], hp->entries[insert_pos]) < 0)
     {
         tmp = hp->entries[parent_pos];
         hp->entries[parent_pos] = hp->entries[insert_pos];
@@ -163,71 +164,68 @@ int heapInsert(heap *hp, heapNode *new) {
 
         // move up one level in the conceptual tree
         insert_pos = parent_pos;
-        parent_pos = heapParent(parent_pos);
+        parent_pos = heapParent(insert_pos);
     }
 
     hp->size++;
-
-    return 1;
 }
 
-heapNode *heapExtract(heap *hp) {
-    if (hp->size == 0)
-        return NULL;
-    heapNode *hn, *save, *tmp;
+void *heapRemove(heap *hp) {
+    void *hn, *save, *tmp;
     int ipos, lpos, rpos, mpos;
 
     hn = hp->entries[0];
-
     save = hp->entries[hp->size - 1];
-    if (hp->size - 1 > 0)
+
+    if ((hp->size - 1) > 0)
         hp->size--;
     else {
-        // we always have a heap size of 1 so there is something to compare
-        // against
+        hp->size=0;
         return hn;
     }
 
     hp->entries[0] = save;
 
     ipos = 0;
+    lpos = heapLeft(ipos);
+    rpos = heapRight(ipos);
     while (1) {
         lpos = heapLeft(ipos);
         rpos = heapRight(ipos);
 
         if (lpos < hp->size &&
-                hp->compare(hp->entries[lpos], hp->entries[ipos])) {
+                // this might be wrong
+                hp->cmp(hp->entries[lpos], hp->entries[ipos]) > 0) {
             mpos = lpos;
-        } else
+        } else {
             mpos = ipos;
+        }
 
         if (rpos < hp->size &&
-                hp->compare(hp->entries[rpos], hp->entries[mpos]))
+                hp->cmp(hp->entries[rpos], hp->entries[mpos]) > 0)
             mpos = rpos;
 
-        if (mpos == ipos)
-            break;
-        else {
-            tmp = hp->entries[mpos];
-            hp->entries[mpos] = hp->entries[ipos];
-            hp->entries[ipos] = tmp;
-            ipos = mpos;
-        }
+        if (mpos == ipos) break;
+        tmp = hp->entries[mpos];
+        hp->entries[mpos] = hp->entries[ipos];
+        hp->entries[ipos] = tmp;
+        ipos = mpos;
     }
     return hn;
 }
 
-void heapPrint(heap *hp) {
-    heapNode *hn;
-    for (int i = 0; i < hp->size; ++i) {
-        hn = hp->entries[i];
-        printf("x:%d, y:%d, weight: %d\n", hn->x, hn->y, hn->weight);
+void heapPrint(heap *h) {
+    for (int i = 0; i < h->size; ++i) {
+        if (h->entries[i])
+            h->print(h->entries[i]);
     }
 }
 
-void heapRelease(heap *hp) {
-    xfree(hp->entries);
-    xfree(hp);
+void heapRelease(heap *h) {
+    for (int i = 0; i < h->size; ++i) {
+        if (h->freeValue)
+            h->freeValue(h->entries[i]);
+    }
 }
 
 void getSuroundingNodes(matrix *m, int x, int y, int array[4]) {
@@ -244,17 +242,17 @@ int dijkstra(matrix *m) {
     int tmparray[4], total_risk, current_risk, weight, risk_matrix_value, x, y,
             shortest_path;
     heapNode *hn = NULL;
-    heap *queue = heapNew(weightcmp);
+    heap *queue = heapNew(weightcmp, xfree);
+    queue->print = heapNodePrint;
     matrix *total_risk_matrix = matrixNew(m->rows, m->columns);
 
-    heapInsert(queue, heapNodeNew(matrixGet(m, 0, 0), 0, 0));
+    heapInsert(queue, heapNodeNew(0, 0, 0));
 
     while (1) {
-        hn = heapExtract(queue);
+        hn = heapRemove(queue);
         x = hn->x;
         y = hn->y;
-        if (queue->size > 1)
-            xfree(hn);
+        xfree(hn);
 
         // we're done!
         if (x == m->columns - 1 && y == m->rows - 1)
@@ -293,22 +291,29 @@ int dijkstra(matrix *m) {
             total_risk = risk_matrix_value + weight;
             current_risk = matrixGet(total_risk_matrix, tmpx, tmpy);
 
-            if (total_risk < (current_risk == 0 ? 100000 : current_risk)) {
+            if (total_risk < (current_risk <= 0 ? 100000 : current_risk)) {
                 matrixSet(total_risk_matrix, tmpx, tmpy, total_risk);
                 heapInsert(queue, heapNodeNew(total_risk, tmpx, tmpy));
             }
         }
     }
+
     heapRelease(queue);
     shortest_path = matrixGet(total_risk_matrix, m->columns - 1, m->rows - 1);
     matrixRelease(total_risk_matrix);
     return shortest_path;
 }
 
+int weightBigger(heapNode *hn1, heapNode *hn2) {
+    if (hn1->weight >= hn2->weight)
+        return 1;
+    else
+        return -1;
+}
+
 int main(void) {
     rFile *rf = rFileRead("./input.txt");
     matrix *m = problemToMatrix(rf->buf, rf->len);
-    matrixprint(m);
     int c1 = dijkstra(m);
     printf("c1: %d\n", c1);
 }
